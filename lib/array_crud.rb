97fc688a7ca49572616212ee1d20e53acb0a2a00
YanChan  Lin <yonalin1@gmail.com>
2017-10-17 19:07:53 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,9 +38,11 @@ array[2]
 end
 
 def retrieve_first_element_from_array(array)
-add_element_to_start_of_array(array, element).index
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-add_element_to_end_of_array(array, element).index
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[-1]
 end
