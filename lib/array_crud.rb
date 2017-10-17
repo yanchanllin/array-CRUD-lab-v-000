ae706d5d43f22c4e15dcb81da43a88ef48b7b915
YanChan  Lin <yonalin1@gmail.com>
2017-10-17 19:00:46 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ end
 def retrieve_element_from_index(array, index_number)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
 index_number =2
-add_element_to_start_of_array(array, 2)
+array(2)
 
 end
 
